0d3690e77372bd5f6fbef465ec317aaeae0fe4d9
Kotletta-TT <assimilation13cd@gmail.com>
2020-11-11 02:17:51 +0300

    add test to create user

--- core/tests.py
+++ core/tests.py
@@ -21,9 +21,21 @@ class UserManagersTests(TestCase):
             pass
         with self.assertRaises(TypeError):
             User.objects.create_user()
-        with self.assertRaises(TypeError):
+        with self.assertRaises(ValueError):
             User.objects.create_user(email='')
         with self.assertRaises(ValueError):
             User.objects.create_user(email='', password='foo')
 
-
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser('super@user.com', 'foo')
+        self.assertEqual(admin_user.email, 'super@user.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        try:
+            self.assertIsNone(admin_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(email='super@user.com', password='foo', is_superuser=False)
